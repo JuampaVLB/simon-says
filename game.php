b391862c56e862fbbfe3b752740af72269ed6f49
JuampaVLB <juampablovillalba25@gmail.com>
2023-08-30 23:48:19 -0300

    first commit

---
+++ game.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simón Dice Game</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+    <script src="assets/js/game.js"></script>
+</head>
+<body>
+    <div class="container">
+        <h1>Simón Dice Game</h1>
+        <!-- Aquí irá la lógica del juego -->
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simon Says</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+</head>
+<body>
+    <div id="container">
+        <div class="flex">
+        <img src="./assets/img/logo.png" class="logo" alt="Logo Simon Says">
+        <img src="./assets/img/simon.png" class="simon" alt="simon">
+        </div>
+        <a href="game.php" class="start-button">Start Game</a>
+    </div>
+</body>
+</html>
